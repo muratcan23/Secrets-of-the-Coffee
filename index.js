c4252b82b6fc81caf0ff34044f55c736c0e4c7a7
muratcan23 <muratal0606@gmail.com>
2024-03-07 02:04:54 +0300

    secret of the coffe

---
+++ index.js
@@ -0,0 +1,37 @@
+import bodyParser from "body-parser";
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+var userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveCoffe") {
+    userIsAuthorised = true;
+  }
+  next();
+}
+app.use(passwordCheck);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  res.sendFile(
+    userIsAuthorised
+      ? `${__dirname}/public/secret.html`
+      : `${__dirname}/public/index.html`
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port : ${port}`);
+});
